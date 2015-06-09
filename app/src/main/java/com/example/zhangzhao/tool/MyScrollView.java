1903ad9c4c38d551de192776de6715d92ff077d7
zhangzhao <zhangzhao0501@sina.cn>
2015-06-09 21:49:10 +0800

    2015-06-09  Add MyScrollView

---
+++ app/src/main/java/com/example/zhangzhao/tool/MyScrollView.java
@@ -0,0 +1,35 @@
+package com.example.zhangzhao.tool;
+
+import android.content.Context;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.widget.ScrollView;
+
+/**
+ * Created by zhangzhao on 15/6/9.
+ */
+public class MyScrollView extends ScrollView{
+
+    GestureDetector mGestureDetector;
+
+    public MyScrollView(Context context) {
+        super(context);
+    }
+
+    public void setmGestureDetector(GestureDetector gestureDetector){
+        this.mGestureDetector = gestureDetector;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        super.onTouchEvent(ev);
+        return mGestureDetector.onTouchEvent(ev);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        mGestureDetector.onTouchEvent(ev);
+        super.dispatchTouchEvent(ev);
+        return true;
+    }
+}
